f99a8b0f271c9df0d7674f0b3968a35208e32e7d
takch02 <lim70kr38@gmail.com>
2025-08-18 00:49:04 +0900

    refector : api.ts 에 Backend URL 상수로 정의 및 products.ts에 API 호출에 적용

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 import type { ProductSearchParams, ProductsResponse } from '@/types/api';
-
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+import { API_BASE_URL } from '@/types/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
